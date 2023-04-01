a8048f64b43f3c99b1a9231df2ad6f625a3971d0
naijwu <jaewuchun@gmail.com>
2023-04-01 13:25:11 -0700

    add title

--- src/screens/Client/Client.tsx
+++ src/screens/Client/Client.tsx
@@ -1,46 +1,7 @@
 import { useState } from 'react'
 import styles from './Client.module.css'
 import axios from 'axios'
-import { Configuration, OpenAIApi } from 'openai'
 import { openai } from '@/utility/openai'
-
-const exampleData = [
-    {
-      "url": "https://www.jaewuchun.com",
-      "pageTitle": "jaewuchun",
-      "h1": "jaewuchun",
-      "first_h2": "",
-      "random_text_from_the_page": "based in Vancouver, I'm a second-year science undergrad @ ubc (currently on gap).when not building mainstream.so, I'm developing for nwPlus ubc or going for a drive.early highschool I built several websites (for conferences, charities, businesses).I have a habit of starting projects randomly (most small, weekend projects).some (larger) projects include internetspace.co🡥, wordseveryday.net🡥, and opengavel.app🡥.I love gamifying everything so these are my stats.these days, I've been learning of and building in crypto & web3.I'm interested in the financial markets, writing, and systems.I like to think and create, I love people and ideas.cheers"
-    },
-    {
-      "url": "https://www.jaewuchun.com/undergrad-at-ubc",
-      "pageTitle": "jaewuchun",
-      "h1": "jaewuchun",
-      "first_h2": "",
-      "random_text_from_the_page": "undergrad @ ubcI hope to major in Computer ScienceSome courses I’ve taken and are taking:CPSC 110: Systematic Program Design (DrRacket, Recursion)CPSC 121: Models of Computation (Logic)CPSC 210: Software Construction (Java)MATH 100: Differential Calculus w/ Physics applications (Calc 1)MATH 101: Integral Calculus w/ Physics applications (Calc 2)ATSC 113: Atmospheric Science18 transfer credits from IB (for physics, english, and economics courses)"
-    },
-    {
-      "url": "https://www.jaewuchun.com/mainstream",
-      "pageTitle": "jaewuchun",
-      "h1": "jaewuchun",
-      "first_h2": "",
-      "random_text_from_the_page": "building mainstream.solaunched closed beta!mainstream is a modular, end-to-end governance platform for DAOs. Discuss proposals, have off-chain voting (Moloch, Snapshot, etc.), and then on-chain voting (Bravo, etc.) all in a package that makes sense and is efficientcurrently closing our pre-seed round (led by Lemniscap, joined by Samsung Next + many amazing angels) to focus on onboarding organizations to the platformmedium to long term, we want to offer deep treasury + delegation insight, as well as explore metagovernance + cross-chain governanceexciting!visit the landing at mainstream.so🡥"
-    },
-    {
-      "url": "https://www.jaewuchun.com/nwplus",
-      "pageTitle": "jaewuchun",
-      "h1": "jaewuchun",
-      "first_h2": "",
-      "random_text_from_the_page": "development director @ nwPlus ubcI push modifications and updates for internal tools (hackathon applicant evaluator),update hacker-facing applications (hacker portal), andcreate and/or update the websites for the three hackathons hosted by nwPlus: nwHacks, HackCamp, and cmd-f*.we work with react and firebase.recently hired two new devs as an incoming (for 2022-23) directorbasically, I push buttons, make life easier, and vibevisit nwPlus at nwplus.io🡥"
-    },
-    {
-      "url": "https://www.jaewuchun.com/websites",
-      "pageTitle": "jaewuchun",
-      "h1": "jaewuchun",
-      "first_h2": "",
-      "random_text_from_the_page": "website creationslooking back, I went kind of hard with making a bunch of websites in highschoolthese are several I've designed and codedint'l youth debate initiativesite was for a debate education-focused non-profithelp the business had a list of gofundme's and resources for the looting in the USpa senate was for my highschool's student governmentnimz kitchen was for a fully custom catering serviceminienterprize 2019site was for Canada's largest high-school case competitionchun2020 was my student gov't campaign websitepacific model ungithub was for a highschool MUN conference <-> 2019site bc forensics leaguesite was for a debate academyinova computer associationsite was for a highschool org that hosted cs-related workshops and eventswish youth network society was for a highschool charity that fundraised for researchprosper vancouvergithub was for another highschool case competition <-> 2018site central model un was for an inaugural MUN day-conference"
-    },
-  ]
 
 const APIFY_API_KEY = process.env.NEXT_PUBLIC_APIFY_API_KEY
 
@@ -138,14 +99,12 @@ const Client = () => {
 
     async function cleanAndAddContext(scrapedArray: any) {
         // temporary 
-        scrapedArray = exampleData
-        
         const cleanedArray = []
 
         if (!scrapedArray) return console.error ('no data to sort')
 
         for (let i = 0; i < scrapedArray.length; i++) {
-            const { url, random_text_from_the_page } = scrapedArray[i]
+            const { url, pageTitle, random_text_from_the_page } = scrapedArray[i]
 
             const pathArray = url.replace(/^(https?:\/\/)?(www\.)?/, '').split('/')
             pathArray.shift()
@@ -153,6 +112,7 @@ const Client = () => {
             const summary = await getSummaryOfText(random_text_from_the_page);
 
             cleanedArray.push({
+                title: pageTitle,
                 path: `/${path}`,
                 summary,
                 content: random_text_from_the_page
