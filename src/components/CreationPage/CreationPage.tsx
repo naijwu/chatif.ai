66c22e280f6bf074cf205a67df013a52d1cb887c
Lincoln Lee <Lsha0730@gmail.com>
2023-04-02 05:49:00 -0700

    chore: refactor out creation page

---
+++ src/components/CreationPage/CreationPage.tsx
@@ -0,0 +1,91 @@
+import React from "react";
+import Button from "../Button/Button";
+import styles from "./CreationPage.module.css";
+import { APIFY_DATA_TIMEOUT } from "@/screens/Client/Client";
+
+type Props = {
+  url: string;
+  setURL: React.Dispatch<React.SetStateAction<string>>;
+  appName: string;
+  setAppName: React.Dispatch<React.SetStateAction<string>>;
+  setIsCreating: React.Dispatch<React.SetStateAction<boolean>>;
+  loading: boolean;
+  handleScrape: () => void;
+};
+
+const CreationPage = ({
+  url,
+  setURL,
+  appName,
+  setAppName,
+  setIsCreating,
+  loading,
+  handleScrape,
+}: Props) => {
+  return (
+    <div>
+      <div className={styles.buttons}>
+        <Button href="#">
+          <div
+            className={styles.buttonInner}
+            onClick={() => {
+              setIsCreating(false);
+            }}
+          >
+            Back
+          </div>
+        </Button>
+      </div>
+
+      <div className={styles.newBot}>
+        <h2>
+          {loading ? `Generating your chatbot` : `Let's generate your chatbot!`}
+        </h2>
+        {loading ? (
+          <>
+            <div className={styles.progress}>
+              <div
+                style={{
+                  transition: `all ${APIFY_DATA_TIMEOUT}s linear`,
+                }}
+                className={`${styles.progressBar} ${
+                  loading ? styles.loading : ""
+                }`}
+              ></div>
+            </div>
+          </>
+        ) : (
+          <>
+            <div className={styles.field}>
+              <strong>Name</strong>
+              <input
+                value={appName}
+                onChange={(e) => {
+                  setAppName(e.target.value);
+                }}
+                type="text"
+              />
+            </div>
+            <div className={styles.field}>
+              <strong>URL</strong>
+              <input
+                value={url}
+                onChange={(e) => {
+                  setURL(e.target.value);
+                }}
+                type="text"
+              />
+            </div>
+            <div className={styles.buttonPane}>
+              <div className={styles.button} onClick={handleScrape}>
+                Generate
+              </div>
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default CreationPage;
